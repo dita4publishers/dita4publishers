4000899a6b9ac4ca8e27269e52ba3d8818b8e1ce
Bertrand Lefort <blefort@uottawa.ca>
2012-04-26 01:19:24 +0000

    first pilot is ok, need to reorganize code to obj literal

--- toolkit_plugins/net.sourceforge.dita4publishers.html5/assets/libs/html5/js/ajaxLoader.js
+++ toolkit_plugins/net.sourceforge.dita4publishers.html5/assets/libs/html5/js/ajaxLoader.js
@@ -24,6 +24,9 @@ $(function() {
       state[ id ] = $(this).attr( 'href' ).replace( /^#/, '' );
       $.bbq.pushState( state );
 
+			$('#left-navigation a').removeClass('active');
+      $(this).addClass('active');
+
       // And finally, prevent the default link click behavior by returning false.
       return false;
     });
@@ -38,6 +41,7 @@ $(function() {
 
     htmlurl = $.bbq.getState( $(this).attr( 'id' ) ) || '';
 		rscript = '/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi';
+
     if( htmlurl != '') {
 
      $.ajax( {
@@ -56,7 +60,47 @@ $(function() {
               responseText = r;
             });
 
-            var content = $("<div>").append(responseText.replace(rscript, "")).find("section").html();
+
+ 						var html = $("<div>").append(responseText.replace(rscript, ""));
+
+            var content = html.find("section");
+            var title = html.find("title");
+
+            $('title').html(title.html());
+
+            content.find("a").each(function(index) {
+            	//var uriInfo = parseURL($(this).attr('href'));
+            	// @todo leave pdf and external intact
+            	var path = htmlurl.substring(0,  htmlurl.lastIndexOf("/"));
+              $(this).attr('href', "#" + path + "/" + $(this).attr('href'));
+
+            	$(this).live( 'click', function(e) {
+     					var state = {};
+
+      				// Get the id of this .bbq widget.
+      				id = 'nav';
+
+     					// Set the state!
+      				state[ id ] = $(this).attr( 'href' ).replace( /^#/, '' );
+      				$.bbq.pushState( state );
+
+      				// And finally, prevent the default link click behavior by returning false.
+     				 return false;
+    				});
+				});
+
+
+    				 content.find("img").each(function(index) {
+            	//var uriInfo = parseURL($(this).attr('href'));
+            	// @todo leave pdf and external intact
+            	var path = htmlurl.substring(0,  htmlurl.lastIndexOf("/")) + "/" +   $(this).attr('src');
+              $(this).attr('src',  path);
+
+
+
+
+
+            });
 
              $("#main-content").html(content);
           }
