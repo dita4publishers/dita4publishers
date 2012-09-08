f5e484f50112dd136abf828545859b79a1fc1346
Bertrand Lefort <blefort@uottawa.ca>
2012-09-08 00:37:06 +0000

    start working on analytics and reltable code

---
+++ toolkit_plugins/net.sourceforge.dita4publishers.html5/libs/html5/js/reltable.js
@@ -0,0 +1,22 @@
+(function (d4h5) {
+
+    // use ui-dialog instead ?
+
+    var reltable = {
+    	// parent topic container
+		parent: {},
+		
+		// next topic
+		next: {},
+		
+		// previous
+		previous: {},
+		
+		
+		
+    };
+
+    d4h5.register('reltable');
+    d4h5.reltable = reltable;
+
+})(d4h5);
