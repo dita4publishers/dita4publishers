06a046909716e2301b419424e00097722f0b041a
Bertrand Lefort <blefort@uottawa.ca>
2014-12-10 21:20:30 -0500

    First attempt to configure traviscl

---
+++ travis-before-install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget http://downloads.sourceforge.net/project/dita-ot/DITA-OT%20Stable%20Release/DITA%20Open%20Toolkit%201.8/DITA-OT1.8.5_standard_bin.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fdita-ot%2Ffiles%2FDITA-OT%2520Stable%2520Release%2FDITA%2520Open%2520Toolkit%25201.8%2F&ts=1418263991&use_mirror=iweb -O ~/dita-ot.tar.gz
+tar -xvf ~/dita-ot.tar.gz
