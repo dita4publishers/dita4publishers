c1f412600e7550061196deba5611dde25e1ef738
Bertrand Lefort <blefort@uottawa.ca>
2014-12-11 08:20:07 -0500

    try to build DITA_OT demo

--- run-tests.sh
+++ run-tests.sh
@@ -1,1 +1,2 @@
 #!/bin/bash
+ant -f DITA-OT1.8.5/build_demo.xml
--- travis-before-install.sh
+++ travis-before-install.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
-wget http://sourceforge.net/projects/dita-ot/files/DITA-OT%20Stable%20Release/DITA%20Open%20Toolkit%201.8/DITA-OT1.8.5_full_easy_install_bin.tar.gz/download -O ~/dita-ot.tar.gz --max-redirect=1
-tar -xvf ~/dita-ot.tar.gz
+wget http://sourceforge.net/projects/dita-ot/files/DITA-OT%20Stable%20Release/DITA%20Open%20Toolkit%201.8/DITA-OT1.8.5_full_easy_install_bin.tar.gz/download -O dita-ot.tar.gz --max-redirect=2
+tar -xvf dita-ot.tar.gz
+cp -r toolkit_plugins/* DITA-OT1.8.5/plugins/
+ant -f DITA-OT1.8.5/integrator.xml
+cd DITA-OT1.8.5
+
+if [ "${DITA_HOME:+1}" = "1" ] && [ -e "$DITA_HOME" ]; then
+  export DITA_DIR="$(realpath "$DITA_HOME")"
+else #elif [ "${DITA_HOME:+1}" != "1" ]; then
+  export DITA_DIR="$(dirname "$(realpath "$0")")"
+fi
+
+if [ -f "$DITA_DIR"/tools/ant/bin/ant ] && [ ! -x "$DITA_DIR"/tools/ant/bin/ant ]; then
+  chmod +x "$DITA_DIR"/tools/ant/bin/ant
+fi
+
+export ANT_OPTS="-Xmx512m $ANT_OPTS"
+export ANT_OPTS="$ANT_OPTS -Djavax.xml.transform.TransformerFactory=net.sf.saxon.TransformerFactoryImpl"
+
+NEW_CLASSPATH="$DITA_DIR/lib/dost.jar"
+NEW_CLASSPATH="$DITA_DIR/lib:$NEW_CLASSPATH"
+NEW_CLASSPATH="$DITA_DIR/lib/commons-codec-1.4.jar:$NEW_CLASSPATH"
+NEW_CLASSPATH="$DITA_DIR/lib/resolver.jar:$NEW_CLASSPATH"
+NEW_CLASSPATH="$DITA_DIR/lib/icu4j.jar:$NEW_CLASSPATH"
+NEW_CLASSPATH="$DITA_DIR/lib/xercesImpl.jar:$NEW_CLASSPATH"
+NEW_CLASSPATH="$DITA_DIR/lib/xml-apis.jar:$NEW_CLASSPATH"
+NEW_CLASSPATH="$DITA_DIR/lib/saxon/saxon9.jar:$NEW_CLASSPATH"
+NEW_CLASSPATH="$DITA_DIR/lib/saxon/saxon9-dom.jar:$NEW_CLASSPATH"
+if test -n "$CLASSPATH"; then
+  export CLASSPATH="$NEW_CLASSPATH":"$CLASSPATH"
+else
+  export CLASSPATH="$NEW_CLASSPATH"
+fi
