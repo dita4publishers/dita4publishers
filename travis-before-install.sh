e6382c3d5b492993f538c090d1dee7d209d778db
Bertrand Lefort <blefort@uottawa.ca>
2014-12-10 21:59:59 -0500

    try to manage redirection

--- travis-before-install.sh
+++ travis-before-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget http://downloads.sourceforge.net/project/dita-ot/DITA-OT%20Stable%20Release/DITA%20Open%20Toolkit%201.8/DITA-OT1.8.5_standard_bin.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fdita-ot%2Ffiles%2FDITA-OT%2520Stable%2520Release%2FDITA%2520Open%2520Toolkit%25201.8%2F&ts=1418263991&use_mirror=iweb -O ~/dita-ot.tar.gz
+wget http://sourceforge.net/projects/dita-ot/files/DITA-OT%20Stable%20Release/DITA%20Open%20Toolkit%201.8/DITA-OT1.8.5_full_easy_install_bin.tar.gz/download -O ~/dita-ot.tar.gz --max-redirect=1
 tar -xvf ~/dita-ot.tar.gz
