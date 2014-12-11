a00d67a9e516d345ea928ebebf1d829844bae502
Bertrand Lefort <blefort@uottawa.ca>
2014-12-11 11:34:06 -0500

    try to fix env

--- travis-before-install.sh
+++ travis-before-install.sh
@@ -4,31 +4,6 @@ tar -xvf dita-ot.tar.gz
 cp -r toolkit_plugins/* DITA-OT1.8.5/plugins/
 ant -f DITA-OT1.8.5/integrator.xml
 cd DITA-OT1.8.5
-
-if [ "${DITA_HOME:+1}" = "1" ] && [ -e "$DITA_HOME" ]; then
-  export DITA_DIR="$(realpath "$DITA_HOME")"
-else #elif [ "${DITA_HOME:+1}" != "1" ]; then
-  export DITA_DIR="$(dirname "$(realpath "$0")")"
-fi
-
-if [ -f "$DITA_DIR"/tools/ant/bin/ant ] && [ ! -x "$DITA_DIR"/tools/ant/bin/ant ]; then
-  chmod +x "$DITA_DIR"/tools/ant/bin/ant
-fi
-
-export ANT_OPTS="-Xmx512m $ANT_OPTS"
-export ANT_OPTS="$ANT_OPTS -Djavax.xml.transform.TransformerFactory=net.sf.saxon.TransformerFactoryImpl"
-
-NEW_CLASSPATH="$DITA_DIR/lib/dost.jar"
-NEW_CLASSPATH="$DITA_DIR/lib:$NEW_CLASSPATH"
-NEW_CLASSPATH="$DITA_DIR/lib/commons-codec-1.4.jar:$NEW_CLASSPATH"
-NEW_CLASSPATH="$DITA_DIR/lib/resolver.jar:$NEW_CLASSPATH"
-NEW_CLASSPATH="$DITA_DIR/lib/icu4j.jar:$NEW_CLASSPATH"
-NEW_CLASSPATH="$DITA_DIR/lib/xercesImpl.jar:$NEW_CLASSPATH"
-NEW_CLASSPATH="$DITA_DIR/lib/xml-apis.jar:$NEW_CLASSPATH"
-NEW_CLASSPATH="$DITA_DIR/lib/saxon/saxon9.jar:$NEW_CLASSPATH"
-NEW_CLASSPATH="$DITA_DIR/lib/saxon/saxon9-dom.jar:$NEW_CLASSPATH"
-if test -n "$CLASSPATH"; then
-  export CLASSPATH="$NEW_CLASSPATH":"$CLASSPATH"
-else
-  export CLASSPATH="$NEW_CLASSPATH"
-fi
+travis env set DITA_DIR DITA-OT1.8.5
+travis env set ANT_OPTS="-Xmx512m $ANT_OPTS -Djavax.xml.transform.TransformerFactory=net.sf.saxon.TransformerFactoryImpl"
+travis env set CLASSPATH DITA-OT1.8.5/lib/dost.jar:DITA-OT1.8.5/lib/commons-codec-1.4.jar:DITA-OT1.8.5/lib/resolver.jar:lib/icu4j.jar:DITA-OT1.8.5/lib/xercesImpl.jar:DITA-OT1.8.5/lib/xml-apis.jar:DITA-OT1.8.5/lib/saxon/saxon9.jar:DITA-OT1.8.5/lib/saxon/saxon9-dom.jar
