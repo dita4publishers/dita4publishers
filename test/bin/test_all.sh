949094c529b639be30107c6b78139cc8edf15dc5
Eliot Kimber <ekimber@contrext.com>
2015-01-13 16:27:12 -0600

    Adjusting 2.0 scripts. 1.8 tests appear to run correctly

--- test/bin/test_all.sh
+++ test/bin/test_all.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # HTML5 transform: 
+cd $DITA_DIR
 echo Running HTML5 transform on complex_map:
-$DITA_DIR/bin/dita -f d4p-html5 -i $DITA_DIR/test/dita/complex_map/complex_map.ditamap -o $DITA_DIR/out/complex_map -temp $DITA_DIR/temp/complex_map
+bin/dita -f d4p-html5 -i $DITA_DIR/test/dita/complex_map/complex_map.ditamap -o out/complex_map 
 
 # EPUB transform:
 echo Running EPUB transform on complex_map:
-$DITA_DIR/bin/dita -f epub -i $DITA_DIR/test/dita/complex_map/complex_map.ditamap -o $DITA_DIR/out/complex_map -temp $DITA_DIR/temp/complex_map
+cd $DITA_DIR
+bin/dita -f epub -i test/dita/complex_map/complex_map.ditamap -o out/complex_map 
 
 # End of script
--- travis/run-tests.sh
+++ travis/run-tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # The command to run is defined in the env in the .travis.yml configuration
 # so that we can either use ant or use the DIT 2.x dita command
-echo Environment:
-env
+echo ls $DITA_DIR/test/bin:
+ls $DITA_DIR/test/bin:
 echo Calling command $CMD...
 $CMD
 echo Done
