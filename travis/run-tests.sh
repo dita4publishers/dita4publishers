7a92c3c4a17c9fbb90fb865094720268b91043df
Eliot Kimber <ekimber@contrext.com>
2015-01-13 16:13:55 -0600

    Trying to get CMD env variable to work

--- travis/run-tests.sh
+++ travis/run-tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # The command to run is defined in the env in the .travis.yml configuration
 # so that we can either use ant or use the DIT 2.x dita command
-echo Calling command "$cmd"...
-$cmd
+echo Environment:
+env
+echo Calling command $CMD...
+$CMD
 echo Done
