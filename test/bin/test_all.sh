f54c5805cecf177f5f5733424fe9109d0acdc13c
Eliot Kimber <ekimber@contrext.com>
2015-01-13 17:01:04 -0600

    OT 2.0 tests run to failure

--- test/bin/test_all.sh
+++ test/bin/test_all.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # HTML5 transform: 
 cd $DITA_DIR
+otdir=`pwd`
 echo Running HTML5 transform on complex_map:
 bin/dita -f d4p-html5 -i $DITA_DIR/test/dita/complex_map/complex_map.ditamap -o out/complex_map 
 
 # EPUB transform:
 echo Running EPUB transform on complex_map:
-cd $DITA_DIR
+cd $otdir
 bin/dita -f epub -i test/dita/complex_map/complex_map.ditamap -o out/complex_map 
 
 # End of script
--- travis/run-tests.sh
+++ travis/run-tests.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 # The command to run is defined in the env in the .travis.yml configuration
 # so that we can either use ant or use the DIT 2.x dita command
-echo ls $DITA_DIR
-ls $DITA_DIR
-ls $DITA_DIR/test
-ls $DITA_DIR/test/bin
-tree $DITA_DIR
 echo Calling command $CMD...
 $CMD
 echo Done
