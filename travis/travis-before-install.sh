55e7010c6e205d7b8a6ab5981f98fa2e7f64b479
Eliot Kimber <ekimber@contrext.com>
2015-01-13 16:55:15 -0600

    Corrected name of 2.0 OT dir

--- travis/travis-before-install.sh
+++ travis/travis-before-install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Install tree command because it's handy
-apt-get install tree
-
 # get DITA_OT
 wget $DITA_REPO -O dita-ot.tar.gz --max-redirect=2
 
 # untar
 tar -xvf dita-ot.tar.gz
+
+echo ls .:
+ls .
 
 # install dp4 plugins (note, this runs the integrator script)
 ant deploy-toolkit-plugins -Ddita-ot-dir=$DITA_DIR
