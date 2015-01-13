307e7d3e578c6cef51fab9f59c01540bacf3ddcf
Eliot Kimber <ekimber@contrext.com>
2015-01-13 16:35:45 -0600

    Fixed dir name for 2.0 OT, trying to figure out why 2.0 script call isn't working

--- travis/run-tests.sh
+++ travis/run-tests.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # The command to run is defined in the env in the .travis.yml configuration
 # so that we can either use ant or use the DIT 2.x dita command
-echo ls $DITA_DIR/test/bin:
-ls $DITA_DIR/test/bin:
+echo ls $DITA_DIR
+ls $DITA_DIR
+tree $DITA_DIR
 echo Calling command $CMD...
 $CMD
 echo Done
