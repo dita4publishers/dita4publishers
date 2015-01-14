32b71afb55fa271015c91a96f67db03b2bfb70fd
Eliot Kimber <ekimber@contrext.com>
2015-01-14 17:11:48 -0600

    Attempt to get failure return code from DITA processes

--- test/bin/test_all.sh
+++ test/bin/test_all.sh
@@ -3,11 +3,17 @@
 cd $DITA_DIR
 otdir=`pwd`
 echo Running HTML5 transform on complex_map:
-bin/dita -f d4p-html5 -i $DITA_DIR/test/dita/complex_map/complex_map.ditamap -o out/complex_map 
-
+bin/dita -f d4p-html5 -i $DITA_DIR/test/dita/complex_map/complex_map.ditamap -o out/complex_map
+r=$?
+echo "$r return code from HTML5 transform"
+rFinal=$r 
 # EPUB transform:
 echo Running EPUB transform on complex_map:
 cd $otdir
 bin/dita -f epub -i test/dita/complex_map/complex_map.ditamap -o out/complex_map 
-
+r=$?
+echo "$r return code from EPUB transform" 
+rFinal=$(($rFinal && $r))
+echo "Final return code is $rFinal" 
+exit $rFinal 
 # End of script
