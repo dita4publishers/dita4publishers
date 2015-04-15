b32d161113ae8d2459a9aa9ac29a0b7f848bc9c3
Bertrand Lefort <blefort@uottawa.ca>
2015-04-15 08:11:14 -0400

    increase verbosity

--- travis/doc-engine-install.sh
+++ travis/doc-engine-install.sh
@@ -2,12 +2,13 @@
 set -e # exit with nonzero exit code if anything fails
 
 SCRIPT_DIR=${PWD##*/} 
+echo "SCRIPT DIR =" $SCRIPT_DIR
 
 export ANT_OPTS=-Xmx2048m
 export DITA_DIR=DITA-OT1.8.5
 export CLASSPATH=$DITA_DIR/lib/:$DITA_DIR/lib/dost.jar:$DITA_DIR/lib/commons-codec-1.4.jar:$DITA_DIR/lib/resolver.jar:lib/icu4j.jar:$DITA_DIR/lib/xercesImpl.jar:$DITA_DIR/lib/xml-apis.jar:$DITA_DIR/lib/saxon/saxon9.jar:$DITA_DIR/lib/saxon/saxon9-dom.jar ANT_HOME=$DITA_DIR/tools/ant PATH=$DITA_DIR/tools/ant/bin:$PATH
 export DITA_REPO=http://sourceforge.net/projects/dita-ot/files/DITA-OT%20Stable%20Release/DITA%20Open%20Toolkit%201.8/DITA-OT1.8.5_full_easy_install_bin.tar.gz/download
 
-ls $SCRIPT_DIR
+ls .
 chmod +x $SCRIPT_DIR/travis-before-install.sh
 bash $SCRIPT_DIR/travis-before-install.sh
