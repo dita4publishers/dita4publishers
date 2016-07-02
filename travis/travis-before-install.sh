e4ea037b6f30a6117dd008388610882ffbdc3aac
Bertrand Lefort <bertrand.lefort@yahoo.fr>
2016-07-02 17:54:04 -0400

    mv to zip, seems that the .tar.gz contains only the source

--- travis/travis-before-install.sh
+++ travis/travis-before-install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # get DITA_OT
-wget $DITA_REPO -O dita-ot.tar.gz --max-redirect=2
+wget $DITA_REPO -O dita-ot.zip --max-redirect=2
 
 # untar
-tar -xvf dita-ot.tar.gz
+#tar -xvf dita-ot.tar.gz
+unzip dita-ot.zip 
+
 
 echo ls .:
 ls .
+pwd
 
 # install dp4 plugins (note, this runs the integrator script)
 ant deploy-toolkit-plugins -Ddita-ot-dir=$DITA_DIR -Dplugin-deploy-clean=false
