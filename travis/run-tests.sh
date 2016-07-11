16663e26010d127fa7ab882b942f05e7ba1d195e
Bertrand Lefort <bertrand.lefort@yahoo.fr>
2016-07-10 21:10:52 -0400

    add -e to bash

--- travis/run-tests.sh
+++ travis/run-tests.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/bash -e
+# ref -e https://groups.google.com/forum/#!topic/travis-ci/JtC4WiCMA0w
 # The command to run is defined in the env in the .travis.yml configuration
 # so that we can either use ant or use the DIT 2.x dita command
 echo Calling command $CMD...
